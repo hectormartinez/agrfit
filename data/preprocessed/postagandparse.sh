56cb56bb5c041dc58df56c70ec21da14689168aa
alonso <alonso@hum.ku.dk>
2015-06-21 21:57:17 +0200

    postag

--- data/preprocessed/postagandparse.sh
+++ data/preprocessed/postagandparse.sh
@@ -8,7 +8,7 @@ parameterfile="/home/alonso/proj/text2depparse/data/$lang/$lang-ud-train.treetag
 parsermodel="/home/alonso/proj/text2depparse/data/$lang/$lang-ud-train.10cols.conll.model"
 
 
-    inputfile=$file
+    inputfile=$file.input
     taggedtemp=$file.postemp
     taggedfile=$file.pos
     parsedfile=$file.conll
@@ -21,7 +21,7 @@ parsermodel="/home/alonso/proj/text2depparse/data/$lang/$lang-ud-train.10cols.co
 cd /home/alonso/tool/TurboParser-2.2.0/
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd;`/deps/local/lib:"
 
-        parsedfile="/home/alonso/proj/framenetxling/data/framenet_annotation_2015/$lang/topos/$file.conll"
+        #parsedfile="/home/alonso/proj/framenetxling/data/framenet_annotation_2015/$lang/topos/$file.conll"
         ./TurboParser --test --file_model=$parsermodel --file_test=$taggedfile  --file_prediction=$parsedfile --logtostderr
         #rm $taggedfile
 
