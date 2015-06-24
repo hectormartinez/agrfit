e2b9e078b9307a14fca4b7aaf018403d79e17461
alonso <alonso@hum.ku.dk>
2015-06-24 12:21:29 +0200

    x

--- source/create_form_lemma_count_dict.py
+++ source/create_form_lemma_count_dict.py
@@ -1,8 +1,12 @@
 import argparse
 
 
+def isvalid(s):
+    current = s.replace("@","A").replace("~","0").replace("-","1").replace("/","2").replace(".","3").replace("_","4")
+    return current.isalpha() or current.isnum() or current.isalnum()
+
 def contentlist(s):
-    return [x for x in s.replace("\n","").split(" ") if x.isalpha()]
+    return [x for x in s.replace("\n","").split(" ") if x.isvalid()]
 
 def main():
     parser = argparse.ArgumentParser(description="Rewrite Semafor input to annotation format")
