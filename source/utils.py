715ba8721fa5ebc9d93e492e1dd865636028d2c6
alonso <alonso@hum.ku.dk>
2015-06-24 14:29:45 +0200

    u

--- source/utils.py
+++ source/utils.py
@@ -14,6 +14,25 @@ PREFERNIVRETAG=[("NOUN","PROPN"),("VERB","AUX"),("X","INTJ")]
 
 
 
+valid = "abcdefghijklmnopqrstuvwzyz0123456789"
+
+def isvalid(s):
+    current = s.replace("@","A").replace("~","0").replace("-","1").replace("/","2").replace(".","3").replace("_","4").lower()
+    current2=""
+    for c in current:
+        if c in valid:
+            current2+=c
+    return current2.isalpha() or current2.isdigit() or current2.isalnum()
+
+
+def contentlist(s):
+    v= [x.lower() for x in s.replace("\n","").split(" ") if isvalid(x)]
+    if len(v)>0:
+        if v[-1] in "?.!":
+            v=v[:-1]
+    return v
+
+
 def cleanheadword(s,key=None):
     s = s.strip()
     if len(s.split(" ")) != 1:
