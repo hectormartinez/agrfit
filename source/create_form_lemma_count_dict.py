b228f35bff98ffe59a1c015cdbd34214125308d3
alonso <alonso@hum.ku.dk>
2015-06-24 11:40:42 +0200

    lemmaform

--- source/create_form_lemma_count_dict.py
+++ source/create_form_lemma_count_dict.py
@@ -14,9 +14,11 @@ def main():
     parser.add_argument("outdict",  metavar="FILE", help="")
     args = parser.parse_args()
     for lineform, linestem in zip(open((args.inform).readlines(),open(args.instem).readlines()))[:30]:
+        print(lineform.split(" "))
+        print(linestem.split(" "))
         print(len(lineform.split(" ")),len(linestem.split(" ")))
 
 
 
 if __name__ == "__main__":
-    main()__author__ = 'alonso'
+    main()
